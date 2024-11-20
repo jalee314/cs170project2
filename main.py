c1e9b5cbec840a2abc84b160d4286f242793e6c0
jalee314 <jasonklee1003@gmail.com>
2024-11-20 10:46:23 -0800

    started basic part

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Jason Lee's Feature Selection Algorithm.\n\n")
+
+num_features = int(input("Please enter total number of features: "))    
+
+algo = int(input("Type the number of the algorithm you want to run.\n\n\t1) Forward Selection\n\t2) Backward Elimination\n\n"))
+
+if algo > 2 or algo < 1:
+    print("Invalid\n")
+    quit()
+
+
+
