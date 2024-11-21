2f2f645578f092c862cfb88b9c232ca9d6aa8046
jalee314 <jasonklee1003@gmail.com>
2024-11-20 20:34:16 -0800

    changed up implementation, added dictionary for easy key value pairing

--- search.py
+++ search.py
@@ -6,25 +6,29 @@ class Problem:
     def __init__(self, algo_choice, num_features):
         self.algo_choice = algo_choice
         self.num_features = num_features
+        self.accuracy_dict = {}
 
-    def algorithm(self, features):
-        if self.algo_choice == 1:                           #forward selection
-        
+    def algorithm(self):
+        if self.algo_choice == 1:                                               #forward selection
+
             explored_features = set()
+            best_subset = set()
             max_accuracy = -1
 
-            for feature in features:
-                if feature in explored_features:
-                    continue
+            no_feature_accuracy = self.evaluation_function(0)
+            max_accuracy = no_feature_accuracy
+
+            print(f"Using no features and \"random\" evaluation, I get an accuracy of{no_feature_accuracy:.2f}%\n")
+
+            
                 
 
-        else:
-            explored_features = set()                       #backward elimination
-                
+    def evaluation_function(self, num_features, subset=None):                   #for number of features we generate, generate a random probability to each
+        if subset is None:
+            subset = tuple()                                                    #base case for empty set
+        if subset not in self.accuracy_dict:
+            accuracy = random.random() * 100
+            self.accuracy_dict[subset] = accuracy
+        return self.accuracy_dict[num_features]
 
-    def evaluation_function(self):                          #for number of features we generate, generate a random probability to each feature
-        feature_list = []
-        for feature in range(self.num_features):
-            feature_list.append(random.random())
-        return feature_list
 
