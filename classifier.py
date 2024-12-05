a43a2344ef467cbade216b08a71044a7a1a3b7ab
jalee314 <jasonklee1003@gmail.com>
2024-12-05 11:47:09 -0800

    added training and constructor functions, normalize function added to normalize features

--- classifier.py
+++ classifier.py
@@ -1,9 +1,23 @@
+import math
+
+
 class KNN:
     def __init__(self):
-        pass
+        self.features = None
+        self.labels = None
 
     def train(self, training_data):
-        pass
+        features = [row[1:] for row in training_data]
+        self.labels = [row[0] for row in training_data]
+        self.features = self.normalize(features)
+
 
     def test(self, test_instance):
+        pass
+    
+
+    def euclidean(self, a, b):
+       pass
+
+    def normalize(self, a, b):
         pass
