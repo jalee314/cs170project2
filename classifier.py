7d0d585388454c36ca3cfa02794b5f6d39c1fa8a
jalee314 <jasonklee1003@gmail.com>
2024-12-05 13:07:54 -0800

    normalize function implemented

--- classifier.py
+++ classifier.py
@@ -19,5 +19,25 @@ class KNN:
     def euclidean(self, a, b):
        pass
 
-    def normalize(self, a, b):
-        pass
+    def normalize(self, features):
+        n = len(features[0])
+
+        normalized_features = []
+
+        for i in range(n):
+            col = [row[i] for row in features]
+            col_min, col_max = min(col), max(col)
+            
+            denom = col_max - col_min
+
+            if denom == 0:
+                normalized_col = [0.0 for _ in col]
+            else:
+                normalized_col = [(value - col_min)/denom for value in col]
+
+            for j, value in enumerate(normalized_col):
+                if len(normalized_features) <= j:
+                    normalized_features.append([]) 
+                normalized_features[j].append(value)
+
+        return normalized_features
