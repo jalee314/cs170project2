83b8a19dcdb770ccbeb082333639a5e47b3adda1
jalee314 <jasonklee1003@gmail.com>
2024-12-06 08:25:59 -0800

    finished classifier class, euclidean and test implemented

--- classifier.py
+++ classifier.py
@@ -1,32 +1,61 @@
 import math
 
 
-class KNN:
+class NN:
     def __init__(self):
         self.features = None
         self.labels = None
+        self.min_max = None
 
     def train(self, training_data):
         features = [row[1:] for row in training_data]
         self.labels = [row[0] for row in training_data]
-        self.features = self.normalize(features)
+        self.features, self.min_max = self.normalize(features)
 
 
     def test(self, test_instance):
-        pass
+        distances = []
+
+        normalized_test = []
+
+        for i in range(len(test_instance)):
+            test_val = test_instance[i]
+            min_val = self.min_max[i][0]
+            max_val = self.min_max[i][1]
+            denom = max_val - min_val
+
+            if denom == 0:
+                normalized_val = 0
+            else:
+                normalized_val = (test_val - min_val)/denom
+
+            normalized_test.append(normalized_val)
+
+        for i, train_instance in enumerate(self.features):
+            dist = self.euclidean(normalized_test, train_instance)
+            distances.append((dist, self.labels[i]))  
+
+        nn = min(distances, key=lambda x: x[0])
+        return nn[1]  
     
 
     def euclidean(self, a, b):
-       pass
+        differences = []
+        for i in range(len(a)):
+            differences.append((a[i]-b[i])**2)
+
+        return math.sqrt(sum(differences))
 
     def normalize(self, features):
         n = len(features[0])
 
         normalized_features = []
+        min_max = []
 
         for i in range(n):
             col = [row[i] for row in features]
             col_min, col_max = min(col), max(col)
+            min_max.append((col_min, col_max))
             
             denom = col_max - col_min
 
@@ -40,4 +69,4 @@ class KNN:
                     normalized_features.append([]) 
                 normalized_features[j].append(value)
 
-        return normalized_features
+        return normalized_features, min_max
